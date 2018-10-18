917cb77fb172ed35cda69aaeea3188d5113bbda4
kevinjared <kevinjaredkip@gmail.com>
2018-10-18 19:50:29 +0300

    hoodwatch views

--- hoodwatch/views.py
+++ hoodwatch/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from hoodwatch.models import Post, Profile
+from django.contrib.auth.models import User
+from .forms import NewPostForm, UserForm, ProfileForm 
+from django.contrib.auth.decorators import login_required
+import datetime as dt
 
-# Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html',{"posts": posts})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,user_id=None):
+    if user_id == None:
+        user_id=request.user.id
+    current_user = User.objects.get(id = user_id)
+    user = current_user
+    images = Post.objects.filter(user=current_user)
+    profile = Profile.objects.all()
+    return render(request, 'profile.html', locals())
+
+@login_required(login_url='/accounts/login')
+def updateprofile(request):
+	if request.method == 'POST':
+		form = ProfileForm(request.POST,request.FILES, instance=request.user.profile)
+		if form.is_valid():
+			form.save()
+			return redirect('profile')
+
+	else:
+			form = ProfileForm()
+	return render(request, 'updateprofile.html',{"form":form })
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+            return redirect('index')
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form":form})
+
+def search_results(request):
+
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profiles = Profile.search_by_user(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"profile": searched_profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
